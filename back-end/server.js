44b2888eda5a7d6026fdeca183b396c5ff548db2
yinhaiying <18840842571@163.com>
2021-02-09 17:15:16 +0800

    后端express搭建服务器

---
+++ back-end/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const products = require("./data/products.js");
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("hello")
+})
+app.get("/api/products", (req, res) => {
+  res.json(products)
+})
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product)
+})
+
+
+app.listen(5000, () => {
+  console.log("5000端口正在运行");
+})

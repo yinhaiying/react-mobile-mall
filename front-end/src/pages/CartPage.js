24787ad306d09ad226b0022eea633052b6634f93
yinhaiying <18840842571@163.com>
2021-02-10 15:25:42 +0800

    购物车页面实现

--- front-end/src/pages/CartPage.js
+++ front-end/src/pages/CartPage.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../store/actions/cartActions.js";
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
+import Message from '../components/Message'
 const CartPage = ({ match, location, history }) => {
   const dispatch = useDispatch();
   const id = match.params.id;
@@ -13,12 +15,91 @@ const CartPage = ({ match, location, history }) => {
       dispatch(addToCart(id, quantity));
     }
   }, [dispatch, id, quantity]);
-  const cart = useSelector((state) => state.cart);
-  console.log("cart:", cart)
+  const { cartItems } = useSelector((state) => state.cart);
+  //购物车移除产品
+  const removerFromCartHandler = (id) => {
+    console.log("删除");
+  }
+  //支付函数
+  const checkoutHandler = () => {
+    history.push('/login?redirect=shipping')
+  }
   return (
-    <div>
-      Cart
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>购物车</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            购物为空<Link to='/'>返回主页</Link>
+          </Message>
+        ) : (
+            <ListGroup variant='flush'>
+              {cartItems.map((item) => (
+                <ListGroup.Item key={item.product}>
+                  <Row>
+                    <Col md={2}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col md={3}>
+                      <Link to={`/products/${item.product}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={2}>{item.price}</Col>
+                    <Col md={2}>
+                      <Form.Control
+                        as='select'
+                        value={item.quantity}
+                        onChange={(e) =>
+                          dispatch(
+                            addToCart(item.product, Number(e.target.value))
+                          )
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((i) => (
+                          <option key={i + 1} value={i + 1}>
+                            {i + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                    <Col>
+                      <Button
+                        type='button'
+                        onClick={() => removerFromCartHandler(item.product)}
+                      >
+                        <svg className="icon" aria-hidden="true">
+                          <use xlinkHref="#icon-delete"></use>
+                        </svg>
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                共计({cartItems.reduce((acc, item) => acc + item.quantity, 0)})个产品
+              </h2>
+              ¥{cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                去支付
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
   )
 }
 

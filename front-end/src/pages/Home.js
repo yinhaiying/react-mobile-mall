d692772fcc3d9bb4a8d52a319958964699a6441d
yinhaiying <18840842571@163.com>
2021-02-10 12:25:49 +0800

    创建loading和message的组件

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios";
-import { Spinner } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../store/actions/productActions.js";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product.js";
-
+import Loading from "../components/Loading.js"
+import Message from "../components/Message.js"
 const Home = () => {
   const dispatch = useDispatch();
   const productList = useSelector(state => state.productList);
@@ -20,10 +20,8 @@ const Home = () => {
       <h1>最新产品</h1>
       {
         loading ? (
-          <Spinner animation="border" role="status">
-            <span className="sr-only">Loading...</span>
-          </Spinner>
-        ) : error ? error : (<Row>
+          <Loading></Loading>
+        ) : error ? (<Message variant="danger">{error}</Message>) : (<Row>
           {products.map((product) =>
             <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
               <Product product={product} />

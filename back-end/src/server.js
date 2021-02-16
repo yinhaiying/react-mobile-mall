994271b07848b88a363607f77959571014e65c72
yinhaiying <18840842571@163.com>
2021-02-16 16:46:11 +0800

    前端打包后端部署

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import bodyParser from "body-parser";
+import path from "path";
 
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
@@ -10,6 +11,7 @@ import productsRoutes from "./routes/products.js";
 import usersRoutes from "./routes/users.js";
 import ordersRoutes from "./routes/orders.js";
 import paymentRoutes from "./routes/payment.js"
+
 
 const app = express();
 dotenv.config();
@@ -21,26 +23,33 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.get("/", (req, res) => {
-  res.send("hello")
-})
+
+
 app.use("/api/products", productsRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/orders", ordersRoutes);
 app.use("/api/payment", paymentRoutes);
 
+
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../front-end/build")));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../front-end', 'build', 'index.html'))
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.send("测试")
+  })
+}
+
+
+
 // 到这里还没有匹配上，说明没有找到资源，因此是404，手动抛出404错误，
 app.use(notFound);
 // 错误处理必须放到最后
 app.use(errorHandler);
-
-
-
-
-
-
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

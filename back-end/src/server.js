57b64a10f62bf5bae2947a2e2947705acfbfec83
yinhaiying <18840842571@163.com>
2021-02-16 18:03:07 +0800

    deploy

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -34,9 +34,9 @@ app.use("/api/payment", paymentRoutes);
 
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "/front-end/build")));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'))
   })
 } else {
   app.get("/", (req, res) => {
